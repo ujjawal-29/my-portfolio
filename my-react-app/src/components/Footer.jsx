1831f651f7349663c78dbc4356edae3cce217c66
ujjawal <rajputujjawal29@gmail.com>
2026-02-06 00:07:34 +0530

    React Portfolio initial push

---
+++ my-react-app/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+ooter.jsx
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Ujjawal Kumar Singh ❤️</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h3>UJJAWAL</h3>
+    </header>
+  );
+}
+
+export default Header;
